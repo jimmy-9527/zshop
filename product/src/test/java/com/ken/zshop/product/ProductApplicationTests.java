bc420ff661bf890b57b61b57020abe7047fd1dc2
Ken <jimmyw3950@gmail.com>
2021-05-11 22:21:48 -0700

    update unit test

--- product/src/test/java/com/ken/zshop/product/ProductApplicationTests.java
+++ product/src/test/java/com/ken/zshop/product/ProductApplicationTests.java
@@ -1,13 +1,13 @@
 package com.ken.zshop.product;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductApplicationTests {
+public class ProductApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
