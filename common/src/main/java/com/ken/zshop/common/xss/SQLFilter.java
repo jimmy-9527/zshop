3658265eb79a17846bef4e1ea4acd1e70d650f54
Ken <jimmyw3950@gmail.com>
2021-05-11 22:37:17 -0700

    update package name

--- common/src/main/java/com/ken/zshop/common/xss/SQLFilter.java
+++ common/src/main/java/com/ken/zshop/common/xss/SQLFilter.java
@@ -6,9 +6,9 @@
  * 版权所有，侵权必究！
  */
 
-package io.renren.common.xss;
+package com.ken.zshop.common.xss;
 
-import io.renren.common.exception.RRException;
+import com.ken.zshop.common.exception.RRException;
 import org.apache.commons.lang.StringUtils;
 
 /**
