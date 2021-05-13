54b9c200f651e950a948ee34c65e9b79f375da1c
Ken <jimmyw3950@gmail.com>
2021-05-12 23:29:54 -0700

    cors

---
+++ gateway/src/main/java/com/ken/zshop/gateway/config/ZShopCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.ken.zshop.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class ZShopCorsConfiguration {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Possibly...
+        // config.applyPermitDefaultValues()
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
--- renren-fast/src/main/java/io/renren/config/CorsConfig.java
+++ renren-fast/src/main/java/io/renren/config/CorsConfig.java
@@ -8,14 +8,14 @@
 
 package io.renren.config;
 
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+// @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
+    // @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
