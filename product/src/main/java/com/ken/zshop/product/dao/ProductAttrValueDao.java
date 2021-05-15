4b6c0c697379249b24abd74aabb7ad694a338bdd
Ken <jimmyw3950@gmail.com>
2021-05-15 16:29:33 -0700

    mybatis attribute value

---
+++ product/src/main/java/com/ken/zshop/product/dao/ProductAttrValueDao.java
@@ -0,0 +1,17 @@
+package com.ken.zshop.product.dao;
+
+import com.ken.zshop.product.entity.ProductAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu
+ * 
+ * @author Ken
+ * @email jimmyw3950@gmail.com
+ * @date 2021-05-15 16:26:19
+ */
+@Mapper
+public interface ProductAttrValueDao extends BaseMapper<ProductAttrValueEntity> {
+	
+}
---
+++ product/src/main/java/com/ken/zshop/product/service/ProductAttrValueService.java
@@ -0,0 +1,20 @@
+package com.ken.zshop.product.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ken.zshop.common.utils.PageUtils;
+import com.ken.zshop.product.entity.ProductAttrValueEntity;
+
+import java.util.Map;
+
+/**
+ * spu
+ *
+ * @author Ken
+ * @email jimmyw3950@gmail.com
+ * @date 2021-05-15 16:26:19
+ */
+public interface ProductAttrValueService extends IService<ProductAttrValueEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+}
+
