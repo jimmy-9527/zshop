2251a30563e327730aaa5b0716f0b1ac92d811a9
Ken <jimmyw3950@gmail.com>
2021-05-15 11:13:41 -0700

    AttrVo

--- product/src/main/java/com/ken/zshop/product/service/impl/AttrServiceImpl.java
+++ product/src/main/java/com/ken/zshop/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.ken.zshop.product.service.impl;
 
+import com.ken.zshop.product.dao.AttrAttrgroupRelationDao;
+import com.ken.zshop.product.entity.AttrAttrgroupRelationEntity;
+import com.ken.zshop.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,10 +16,14 @@ import com.ken.zshop.common.utils.Query;
 import com.ken.zshop.product.dao.AttrDao;
 import com.ken.zshop.product.entity.AttrEntity;
 import com.ken.zshop.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+
+    @Autowired
+    private AttrAttrgroupRelationDao attrAttrgroupRelationDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -26,4 +35,16 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         return new PageUtils(page);
     }
 
+    @Transactional
+    @Override
+    public void saveAttr(AttrVo attr) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attr,attrEntity);
+        this.save(attrEntity);
+        //保存attrgroupId
+        AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+        attrAttrgroupRelationEntity.setAttrGroupId(attr.getAttrGroupId()); //属性分组ID
+        attrAttrgroupRelationEntity.setAttrId(attrEntity.getId()); //属性ID
+        attrAttrgroupRelationDao.insert(attrAttrgroupRelationEntity);
+    }
 }
