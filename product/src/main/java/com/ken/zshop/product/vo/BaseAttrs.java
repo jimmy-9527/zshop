7f5e73a2cdc877c7e64b5969357bf24f258e4aa0
Ken <jimmyw3950@gmail.com>
2021-05-15 16:16:17 -0700

    bejson

---
+++ product/src/main/java/com/ken/zshop/product/vo/BaseAttrs.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2019 bejson.com
+ */
+package com.ken.zshop.product.vo;
+
+import lombok.Data;
+
+
+@Data
+public class BaseAttrs {
+
+    private Long attrId;
+    private String attrValues;
+    private int showDesc;
+
+
+
+}
---
+++ product/src/main/java/com/ken/zshop/product/vo/Images.java
@@ -0,0 +1,12 @@
+package com.ken.zshop.product.vo;
+
+import lombok.Data;
+
+@Data
+public class Images {
+
+    private String imgUrl;
+    private int defaultImg;
+
+
+}
