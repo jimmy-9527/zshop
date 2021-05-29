28320132c71a9163d9e2c0fe03bb7b83eae83916
Ken <jimmyw3950@gmail.com>
2021-05-29 08:05:20 -0700

    empty auth2

---
+++ auth2/src/main/java/com.ken.zshop.user2/AuthApplication2.java
@@ -0,0 +1,15 @@
+package com.ken.zshop.user2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class AuthApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication2.class, args);
+	}
+}
